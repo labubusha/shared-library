f26cb39304791d182651ba772d7e6587a9805b3d
Strebkova Anastasiia <astrebkova@Saber3D.net>
2025-09-11 14:01:53 +0300

    change aborted var

--- vars/log.groovy
+++ vars/log.groovy
@@ -21,14 +21,14 @@ def aborted(CHANGE, SHELVE, BOT_TOKEN, CHAT_ID) {
 
 def failed(CHANGE, SHELVE, STATUS, BOT_TOKEN, CHAT_ID) {
     powershell """
-                    \$change = "${CHANGE}"
-                    \$shelve = "${SHELVE}"
-                    echo \$shelve
-                    \$config = \$env:VS_CONFIG.Replace('+', '%2B')
-                    \$emoji = [char]::ConvertFromUtf32(0x274C)
-                    \$message = "\$emoji\$emoji\$emoji <b>FAILURE</b> \$emoji\$emoji\$emoji `n`r`n`r<b>Type</b> - \$env:JOB_BASE_NAME `n`r<b>Platform</b> - \$env:PLATFORM `n`r<b>Target</b> - \$env:BUILD_TARGET `n`r<b>Configuration</b> - \$config `n`r<b>Branch</b> - \$env:BRANCH `n`r<b>Number</b> - \$env:BUILD_ID`n`r<b>Changelist</b> - \$change `n`r<b>SHELVE</b> - \$shelve `n`r<b>Failed at step</b> - ${STATUS}"
-                    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-                    """
+        \$change = "${CHANGE}"
+        \$shelve = "${SHELVE}"
+        echo \$shelve
+        \$config = \$env:VS_CONFIG.Replace('+', '%2B')
+        \$emoji = [char]::ConvertFromUtf32(0x274C)
+        \$message = "\$emoji\$emoji\$emoji <b>FAILURE</b> \$emoji\$emoji\$emoji `n`r`n`r<b>Type</b> - \$env:JOB_BASE_NAME `n`r<b>Platform</b> - \$env:PLATFORM `n`r<b>Target</b> - \$env:BUILD_TARGET `n`r<b>Configuration</b> - \$config `n`r<b>Branch</b> - \$env:BRANCH `n`r<b>Number</b> - \$env:BUILD_ID`n`r<b>Changelist</b> - \$change `n`r<b>SHELVE</b> - \$shelve `n`r<b>Failed at step</b> - ${STATUS}"
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    """
 }
 
 //\$Response = Invoke-RestMethod -Uri "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=\$(\${message})&parse_mode=HTML"
