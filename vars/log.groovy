2b3d47c0bcfd010d7d0b6c488e64749f1c4ebe39
Strebkova Anastasiia <astrebkova@Saber3D.net>
2025-09-11 11:50:49 +0300

    change aborted vars

--- vars/log.groovy
+++ vars/log.groovy
@@ -2,16 +2,16 @@ def info(message) {
     echo "INFO: ${message}"
 }
 
-def aborted(message) {
+def aborted(change, shelve, bot_token, chat_id) {
     echo 'aborted!'
     powershell """
-        \$change = "CHANGE"
-        \$shelve = "SHELVE"
+        \$change = "${CHANGE}"
+        \$shelve = "${SHELVE}"
         echo \$shelve
         \$config = \$env:VS_CONFIG.Replace('+', '%2B')
         \$emoji = [char]::ConvertFromUtf32(0x2716)
         \$message = "\$emoji\$emoji\$emoji <b>ABORTED</b> \$emoji\$emoji\$emoji `n`r`n`r<b>Type</b> - \$env:JOB_BASE_NAME `n`r<b>Platform</b> - \$env:PLATFORM `n`r<b>Target</b> - \$env:BUILD_TARGET `n`r<b>Configuration</b> - \$config `n`r<b>Branch</b> - \$env:BRANCH `n`r<b>Number</b> - \$env:BUILD_ID`n`r<b>Changelist</b> - \$change `n`r<b>SHELVE</b> - \$shelve"
         [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-        \$Response = Invoke-RestMethod -Uri "https://api.telegram.org/botBOT_TOKEN/sendMessage?chat_id=CHAT_ID&text=\$(\$message)&parse_mode=HTML"
+        \$Response = Invoke-RestMethod -Uri "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=\$(\$message)&parse_mode=HTML"
     """
 }
