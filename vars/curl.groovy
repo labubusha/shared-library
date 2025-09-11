5af6e323f732b508bad18799a1f7375624d1db37
Strebkova Anastasiia <astrebkova@Saber3D.net>
2025-09-11 17:35:47 +0300

    add test functions

--- vars/curl.groovy
+++ vars/curl.groovy
@@ -1,6 +1,25 @@
-def curl_cred(items, logFileName) {
+def curl_cred(curl_items, logFileName) {
     bat """
-        url -m 600 -X POST https://${items.user}:${items.token}@${items.jenkins_url}/job/${items.JOB_NAME}/${items.BUILD_ID}/consoleText > ${logFileName} 2>&1
+        url -m 600 -X POST https://${curl_items.user}:${curl_items.token}@${curl_items.jenkins_url}/job/${curl_items.JOB_NAME}/${curl_items.BUILD_ID}/consoleText > ${logFileName} 2>&1
         exit /b 0
     """
+}
+
+def curl_file_size(main_items, logFileName) {
+    def fileSizeInBytes = powershell(returnStdout: true, script: "(Get-Item '${logFileName}').Length")
+    def fileSize = fileSizeInBytes.toInteger()
+    def fileSizeInMB = fileSize / (1024 * 1024)
+    if (fileSizeInMB < 50)
+        {
+            bat """
+            curl -X POST "https://api.telegram.org/bot${main_items.BOT_TOKEN}/sendDocument" -F chat_id=${main_items.CHAT_ID} -F document="@${logFileName}"
+            """
+        }
+        else
+        {
+            bat """
+            "C:\\Program Files\\7-Zip\\7z.exe" a -t7z ${logFileName}.7z ${logFileName}
+            curl -X POST "https://api.telegram.org/bot${main_items.BOT_TOKEN}/sendDocument" -F chat_id=${main_items.CHAT_ID} -F document="@${logFileName}.7z"
+            """
+    }
 }
