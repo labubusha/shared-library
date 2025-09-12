9835f5eddbc9a6aa5b25c218598397540df785f3
Strebkova Anastasiia <astrebkova@Saber3D.net>
2025-09-12 16:17:54 +0300

    add notify send log functions

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -1,10 +1,10 @@
 def bot_send_message(main_items, STATUS = 'None') {
-    def helper = ""
+    def helpString = ""
     def emoji = "[char]::ConvertFromUtf32(0x2716)"
     def type =  "<b>ABORTED</b>"
     if ( STATUS != 'None' ) {
         emoji = "[char]::ConvertFromUtf32(0x274C)"
-        helper = " `n`r<b>Failed at step</b> - ${STATUS}"
+        helpString = " `n`r<b>Failed at step</b> - ${STATUS}"
         type = "<b>FAILURE</b>"
     }
     powershell """
@@ -13,8 +13,28 @@ def bot_send_message(main_items, STATUS = 'None') {
         echo \$shelve
         \$config = \$env:VS_CONFIG.Replace('+', '%2B')
         \$emoji = ${emoji}
-        \$message = "\$emoji\$emoji\$emoji ${type} \$emoji\$emoji\$emoji `n`r`n`r<b>Type</b> - \$env:JOB_BASE_NAME `n`r<b>Platform</b> - \$env:PLATFORM `n`r<b>Target</b> - \$env:BUILD_TARGET `n`r<b>Configuration</b> - \$config `n`r<b>Branch</b> - \$env:BRANCH `n`r<b>Number</b> - \$env:BUILD_ID`n`r<b>Changelist</b> - \$change `n`r<b>SHELVE</b> - \$shelve${helper}"
+        \$message = "\$emoji\$emoji\$emoji ${type} \$emoji\$emoji\$emoji `n`r`n`r<b>Type</b> - \$env:JOB_BASE_NAME `n`r<b>Platform</b> - \$env:PLATFORM `n`r<b>Target</b> - \$env:BUILD_TARGET `n`r<b>Configuration</b> - \$config `n`r<b>Branch</b> - \$env:BRANCH `n`r<b>Number</b> - \$env:BUILD_ID`n`r<b>Changelist</b> - \$change `n`r<b>SHELVE</b> - \$shelve${helpString}"
         [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
         \$Response = Invoke-RestMethod -Uri "https://api.telegram.org/bot${main_items.BOT_TOKEN}/sendMessage?chat_id=${main_items.CHAT_ID}&text=\$(\${message})&parse_mode=HTML"
     """
+}
+
+def send_log(main_items, logFileName) {
+    //def fileSizeInBytes = powershell(returnStdout: true, script: "(Get-Item '${logFileName}').Length")
+    def fileSizeInBytes = 263
+    def fileSize = fileSizeInBytes.toInteger()
+    def fileSizeInMB = fileSize / (1024 * 1024)
+    if (fileSizeInMB < 50)
+    {
+        bat """
+            curl -X POST "https://api.telegram.org/bot${main_items.BOT_TOKEN}/sendDocument" -F chat_id=${main_items.CHAT_ID} -F document="@${logFileName}"
+        """
+    }
+    else
+    {
+        bat """
+            "C:\\Program Files\\7-Zip\\7z.exe" a -t7z ${logFileName}.7z ${logFileName}
+            curl -X POST "https://api.telegram.org/bot${main_items.BOT_TOKEN}/sendDocument" -F chat_id=${main_items.CHAT_ID} -F document="@${logFileName}.7z"
+        """
+    }
 }
