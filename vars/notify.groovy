ad88601fb3436d0d766e80530d78cf4a9c4706ea
Strebkova Anastasiia <astrebkova@Saber3D.net>
2025-09-12 14:40:25 +0300

    add notify send functions

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -1,6 +1,8 @@
 def bot_send_message(main_items, STATUS = 'None') {
-    helper = ""
+    def helper = ""
+    def emoji = "[char]::ConvertFromUtf32(0x2716)"
     if ( STATUS != 'None' ) {
+        emoji = "[char]::ConvertFromUtf32(0x274C)"
         helper = " `n`r<b>Failed at step</b> - ${STATUS}"
     }
     powershell """
@@ -8,7 +10,7 @@ def bot_send_message(main_items, STATUS = 'None') {
         \$shelve = "${main_items.SHELVE}"
         echo \$shelve
         \$config = \$env:VS_CONFIG.Replace('+', '%2B')
-        \$emoji = [char]::ConvertFromUtf32(0x274C)
+        \$emoji = ${emoji}
         \$message = "\$emoji\$emoji\$emoji <b>FAILURE</b> \$emoji\$emoji\$emoji `n`r`n`r<b>Type</b> - \$env:JOB_BASE_NAME `n`r<b>Platform</b> - \$env:PLATFORM `n`r<b>Target</b> - \$env:BUILD_TARGET `n`r<b>Configuration</b> - \$config `n`r<b>Branch</b> - \$env:BRANCH `n`r<b>Number</b> - \$env:BUILD_ID`n`r<b>Changelist</b> - \$change `n`r<b>SHELVE</b> - \$shelve${helper}"
         [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
         \$Response = Invoke-RestMethod -Uri "https://api.telegram.org/bot${main_items.BOT_TOKEN}/sendMessage?chat_id=${main_items.CHAT_ID}&text=\$(\${message})&parse_mode=HTML"
