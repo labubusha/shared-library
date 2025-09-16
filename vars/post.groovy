4d658448db02a25876c0a009c7bb1cdc99b4124a
Strebkova Anastasiia <astrebkova@Saber3D.net>
2025-09-16 17:11:07 +0300

    add test post functions

--- vars/post.groovy
+++ vars/post.groovy
@@ -1,41 +1,5 @@
-def aborted(main_items) {
-    echo 'aborted!'
-    powershell """
-        \$change = "${main_items.CHANGE}"
-        \$shelve = "${main_items.SHELVE}"
-        echo \$shelve
-        \$config = \$env:VS_CONFIG.Replace('+', '%2B')
-        \$emoji = [char]::ConvertFromUtf32(0x2716)
-        \$message = "\$emoji\$emoji\$emoji <b>ABORTED</b> \$emoji\$emoji\$emoji `n`r`n`r<b>Type</b> - \$env:JOB_BASE_NAME `n`r<b>Platform</b> - \$env:PLATFORM `n`r<b>Target</b> - \$env:BUILD_TARGET `n`r<b>Configuration</b> - \$config `n`r<b>Branch</b> - \$env:BRANCH `n`r<b>Number</b> - \$env:BUILD_ID`n`r<b>Changelist</b> - \$change `n`r<b>SHELVE</b> - \$shelve"
-        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-        \$Response = Invoke-RestMethod -Uri "https://api.telegram.org/bot${main_items.BOT_TOKEN}/sendMessage?chat_id=${main_items.CHAT_ID}&text=\$(\$message)&parse_mode=HTML"         
-    """
-}
-
-
-def failed(main_items, STATUS) {
-    powershell """
-        \$change = "${main_items.CHANGE}"
-        \$shelve = "${main_items.SHELVE}"
-        echo \$shelve
-        \$config = \$env:VS_CONFIG.Replace('+', '%2B')
-        \$emoji = [char]::ConvertFromUtf32(0x274C)
-        \$message = "\$emoji\$emoji\$emoji <b>FAILURE</b> \$emoji\$emoji\$emoji `n`r`n`r<b>Type</b> - \$env:JOB_BASE_NAME `n`r<b>Platform</b> - \$env:PLATFORM `n`r<b>Target</b> - \$env:BUILD_TARGET `n`r<b>Configuration</b> - \$config `n`r<b>Branch</b> - \$env:BRANCH `n`r<b>Number</b> - \$env:BUILD_ID`n`r<b>Changelist</b> - \$change `n`r<b>SHELVE</b> - \$shelve `n`r<b>Failed at step</b> - ${STATUS}"
-        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-        \$Response = Invoke-RestMethod -Uri "https://api.telegram.org/bot${main_items.BOT_TOKEN}/sendMessage?chat_id=${main_items.CHAT_ID}&text=\$(\${message})&parse_mode=HTML"
-    """
-}
-
-
-
-def error_message(htmlMessage, main_items) {
-    powershell """
-        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-        \$botToken = "${main_items.BOT_TOKEN}"
-        \$chatId = "${main_items.CHAT_ID}"
-        \$encodedMessage = [uri]::EscapeDataString('${htmlMessage}')
-        \$uri = "https://api.telegram.org/bot\$botToken/sendMessage?chat_id=\$chatId&text=\$encodedMessage&parse_mode=HTML"
-        \$Response = Invoke-RestMethod -Uri \$uri -Method Get
-        Start-Sleep -Seconds 1
-    """
+def test(Map args = [:], main_items) {
+    defaultValues = [STATUS: 'None', PING: 'None']
+    args = defaultValues << args
+    return "${STATUS} ${PING}"
 }
