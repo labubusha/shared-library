3542239b6c397e5a5fa1dd421a546265ad41a3b6
Strebkova Anastasiia <astrebkova@Saber3D.net>
2025-09-17 11:23:50 +0300

    add test post functions

--- vars/post.groovy
+++ vars/post.groovy
@@ -1,6 +1,5 @@
 def call(Map args = [:]) {
     def defaultValues = [STATUS: 'None', PING: 'None', NUMBER: '\$env:BUILD_ID', STEAM_BRANCH_STRING: '']
     def config = defaultValues << args
-    
-    return config.STATUS, config.PING, config.NUMBER, config.STEAM_BRANCH_STRING
+    return config
 }
