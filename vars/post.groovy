5aeea0864353319f92a7d13178cc0ee09576524f
Strebkova Anastasiia <astrebkova@Saber3D.net>
2025-09-17 10:43:35 +0300

    add test post functions

--- vars/post.groovy
+++ vars/post.groovy
@@ -1,7 +1,6 @@
 def call(Map args = [:]) {
-    def defaultValues = [STATUS: 'None', PING: 'None']
+    def defaultValues = [STATUS: 'None', PING: 'None', NUMBER: '\$env:BUILD_ID', STEAM_BRANCH_STRING: '']
     def config = defaultValues << args
     
-    echo "STATUS: ${config.STATUS}"
-    echo "PING: ${config.PING}"
+    return config.STATUS, config.PING, config.NUMBER, config.STEAM_BRANCH_STRING
 }
