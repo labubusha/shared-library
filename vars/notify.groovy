4a877baa48a827fcf22f31cc18b4ffc8796c0b9a
Strebkova Anastasiia <astrebkova@Saber3D.net>
2025-09-22 14:11:43 +0300

    change optional param number

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -59,6 +59,7 @@ def bot_send_message(Map parameters, result) {
     }
 
     if ( parameters.containsKey("config") ) {
+        print parameters.config
         message.config = " `n`r<b>Configuration</b> - ${parameters.config.replace(" ", "+")}"
         print message.config
     }
