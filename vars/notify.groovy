71bc46f2fe3ccb2e330de9b0fbfdfa9e9aeff0d5
Strebkova Anastasiia <astrebkova@Saber3D.net>
2025-09-24 18:27:45 +0300

    add test condition

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -115,7 +115,7 @@ private def send_log_bat(main_items, logFileName, Boolean get7z = false) {
         """
     } else {
          bat """
-            "C:\\Program Files\\7-Zip\\7z.exe" a -t7z ${logFileName}.7z ${logFileName}
+            "C:\\Program Files\\7-Zip\\7z.exe" a -t7z ${logFileName.replace(".txt","")}.7z ${logFileName}
             curl -X POST "https://api.telegram.org/bot${main_items.bot_token}/sendDocument" -F chat_id=${main_items.chat_id} -F document="@${logFileName}.7z"
         """
     }
